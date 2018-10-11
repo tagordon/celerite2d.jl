bbb19096cf3bbb081f81af03111eb3a0a46f6f0f
Tyler Gordon <tagordon@uw.edu>
2018-10-11 15:03:13 -0700

    add .ci

---
+++ .ci/travis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+if [[ $TEST_LANG == paper ]]
+then
+  if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'paper/'
+  then
+    echo "Building the paper..."
+    export CELERITE_BUILDING_PAPER=true
+    source "$( dirname "${BASH_SOURCE[0]}" )"/setup-tectonic.sh
+    return
+  fi
+  export CELERITE_BUILDING_PAPER=false
+  return
+fi
