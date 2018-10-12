ba8df82afc28c78f8c50d2a376241ea4fd7b3f7e
Tyler Gordon <tagordon@uw.edu>
2018-10-12 08:32:23 -0700

    don't recurse

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -8,6 +8,8 @@ make
 if [ -n "$GH_TOKEN" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
+  # next line very important 
+  git rm -rf .
   git add -f paper/paper.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
   git push -q -f https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
