fd890568b3f8c5bfbf39064bc92118abba321849
Tyler Gordon <tagordon@uw.edu>
2018-10-11 18:53:16 -0700

    bleh

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -7,7 +7,8 @@ make
 # Push to GitHub
 if [ -n "$GH_TOKEN" ]; then
   cd $TRAVIS_BUILD_DIR
+  git checkout --orphan $TRAVIS_BRANCH-pdf
   git add -f paper/paper.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-  git push -q -f https://$GITHUB_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG
+  git push -q -f https://$GITHUB_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
