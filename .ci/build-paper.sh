cae4da7a4cc9cfb64c5649b52461684e9fa923cd
Tyler Gordon <tagordon@uw.edu>
2018-10-11 18:13:10 -0700

    fix

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -11,5 +11,5 @@ if [ -n "$GH_TOKEN" ]; then
   git rm -rf .
   git add -f paper/paper.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
-  git push -q -f https://$GITHUB_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+  git push -q -f https://$GITHUB_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH
 fi
