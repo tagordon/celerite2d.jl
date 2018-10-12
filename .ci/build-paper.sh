4becbb0f085ef6e39e1c48a73e87464aa1f33aa7
Tyler Gordon <tagordon@uw.edu>
2018-10-11 17:13:24 -0700

    fixed paper name

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -9,7 +9,7 @@ if [ -n "$GH_TOKEN" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f paper/ms.pdf
+  git add -f paper/paper.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
   git push -q -f https://$GITHUB_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
